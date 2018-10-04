d9e9f118ba37a13edf2811bf2c423184a7f62aa4
carolsancos <carol.sancos@gmail.com>
2018-10-04 10:13:33 -0400

    Title
    Node course

---
+++ react/review-using-props-with-stateless-functional-components.js
@@ -0,0 +1,26 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
+
+const Camper = (props) => {
+  return <p>{props.name}</p>
+};
+
+Camper.defaultProps = {
+  name: 'CamperBot'
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
